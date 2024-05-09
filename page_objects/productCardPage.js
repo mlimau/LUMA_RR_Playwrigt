85a53fa46ed9035ee40c0c7577af741e521afd35
Natalia Vasylieva <123753026+NatalyaVasylyeva@users.noreply.github.com>
2024-05-09 07:38:35 +0200

    Verify Product Card in the Related Products section opens correct page (#544)

--- page_objects/productCardPage.js
+++ page_objects/productCardPage.js
@@ -1,20 +1,38 @@
 import MenTopsPage from '../page_objects/menTopsPage.js';
 
 class ProductCardPage {
-    constructor(page) {
-        this.page = page;
-    }
+  constructor(page) {
+    this.page = page;
+  }
 
-    locators = {
-        getRelatedProductsSection: () => this.page.locator('div.block.related'),
-        getRelatedProductsSectionTitle: () => this.page.locator('#block-related-heading'),
-    };
+  locators = {
+    getRelatedProductsSection: () => this.page.locator("div.block.related"),
+    getRelatedProductsSectionTitle: () =>
+      this.page.locator("#block-related-heading"),
+    getListOfRelatedProductsTitles: () =>
+      this.page.locator("strong.product.name.product-item-name a"),
+    getRelatedProductsTitle: (i) =>
+      this.page.locator("strong.product.name.product-item-name a").nth(i),
+    getProductCardTitile: () => this.page.locator("h1.page-title"),
+  };
 
-    async goBackToMenTopsPage() {
-       await this.page.goBack();
+  async goBackToMenTopsPage() {
+    await this.page.goBack();
 
-       return new MenTopsPage(this.page);
-    }
+    return new MenTopsPage(this.page);
+  }
+
+  async openRelatedProductCard(idx) {
+    await this.locators.getRelatedProductsTitle(idx).click();
+
+    return new ProductCardPage(this.page);
+  }
+
+  async goBackToProductCardPage() {
+    await this.page.goBack();
+
+    return new ProductCardPage(this.page);
+  }
 }
 
 export default ProductCardPage;
