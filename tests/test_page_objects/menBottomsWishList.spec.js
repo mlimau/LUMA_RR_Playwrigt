89d0efc4389b9846f58b71c3e9d893b54265825b
Katerina Poliit <114866316+Katerina-Poliit@users.noreply.github.com>
2024-05-09 15:51:07 +0300

    ERR | menBottomsWishList.spec | should be an "Add to Cart" button (#551)

--- tests/test_page_objects/menBottomsWishList.spec.js
+++ tests/test_page_objects/menBottomsWishList.spec.js
@@ -50,7 +50,7 @@ test.describe('menBottomWishList.spec', () => {
     await expect(wishListPage.locators.getTitleMyWishList()).toHaveCSS('text-align', 'start');
   })
 
-  test.skip('should be a delete item button, a cross', async ({ page }) => {
+  test('should be a delete item button, a cross', async ({ page }) => {
     const homePage = new HomePage(page);
 
     await homePage.hoverMenLink();
@@ -64,7 +64,7 @@ test.describe('menBottomWishList.spec', () => {
     await expect(wishListPage.locators.getTitleNoItems()).toBeVisible();
   })
 
-  test.skip(' should be an "Add to Cart" button', async ({ page }) => {
+  test(' should be an "Add to Cart" button', async ({ page }) => {
     const homePage = new HomePage(page);
 
     await homePage.hoverMenLink();
