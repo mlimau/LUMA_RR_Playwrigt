fd24adfd05992a6763e8e0c9ae8b2ad1cb509708
Yauheni Umetski <146204954+yauheniumetski@users.noreply.github.com>
2024-05-10 12:19:21 +0300

    RF_POM | menPage.spec | Structure refactoring for file (#550)
    RF_POM | menPage.spec | Structure refactoring for file

--- tests/test_page_objects/menPage.spec.js
+++ tests/test_page_objects/menPage.spec.js
@@ -1,108 +1,83 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import {
-    BASE_URL,
-    MEN_PAGE_BOTTOMS_SUB_CATEGORY_LINK_COLOR,
-    MEN_PAGE_SHOP_BY_CATEGORY_BLOCK_ALIGNMENT,
-    MEN_PAGE_SHOP_BY_CATEGORY_SUB_CATEGORIES_AMOUNT,
-    MEN_PAGE_TOPS_SUB_CATEGORY_LINK_COLOR,
-    MEN_PAGE_SHOP_BY_CATEGORY_SUB_CATEGORIES_VALUES_REGEX,
-    MEN_PAGE_SHOP_BY_CATEGORY_SUB_CATEGORIES_COUNTER_DATATYPE,
-    MEN_BOTTOMS_PAGE_END_POINT,
-    MEN_PAGE_SUB_CATEGORY_ENDPOINT_URL
-} from "../../helpers/testData.js";
-import {
-    MEN_PAGE_END_POINT,
-    MEN_PAGE_HEADER,
-    COMPARE_PRODUCTS_TEXT,
-    MY_WISH_LIST_TEXT,
-    HOT_SELLERS_NAME,
-    HOT_SELLERS_ENDPOINT_URL
-} from "../../helpers/testMenData.js";
-import MenPage from "../../page_objects/menPage";
-import MenBottomsPage from "../../page_objects/menBottomsPage.js";
+import * as TEST_DATA from "../../helpers/testData.js";
+import * as TEST_MEN_DATA from "../../helpers/testMenData.js";
 
 test.describe('menPage.spec', () => {
     test.beforeEach(async ({ page }) => {
         const homePage = new HomePage(page);
         await homePage.open();
     });
+
     test('Menu/Men available to click, see clothes only for men', async ({ page }) => {
         const homePage = new HomePage(page);
-        const menPage = new MenPage(page);
+        const menPage = await homePage.clickMenLink();
 
-        await homePage.clickMenLink();
-
-        await expect(page).toHaveURL(BASE_URL + MEN_PAGE_END_POINT);
-        await expect(menPage.locators.getMenPageHeader()).toContainText(MEN_PAGE_HEADER);
-        await expect(menPage.locators.getCompareProducts()).toBeVisible(COMPARE_PRODUCTS_TEXT);
-        await expect(menPage.locators.getMyWishList()).toBeVisible(MY_WISH_LIST_TEXT);
-    })
+        await expect(page).toHaveURL(TEST_DATA.BASE_URL + TEST_MEN_DATA.MEN_PAGE_END_POINT);
+        await expect(menPage.locators.getMenPageHeader()).toContainText(TEST_MEN_DATA.MEN_PAGE_HEADER);
+        await expect(menPage.locators.getCompareProducts()).toBeVisible(TEST_MEN_DATA.COMPARE_PRODUCTS_TEXT);
+        await expect(menPage.locators.getMyWishList()).toBeVisible(TEST_MEN_DATA.MY_WISH_LIST_TEXT);
+    });
 
     test('Men page contains Shop by category block which is located on the left side of the page', async ({ page }) => {
         const homePage = new HomePage(page);
-        const menPage = new MenPage(page);
-
-        await homePage.clickMenLink();
+        const menPage = await homePage.clickMenLink();
 
         await expect(menPage.locators.getShopByCategoryBlock()).toBeVisible();
-        await expect(menPage.locators.getShopByCategoryBlock()).toHaveCSS('float', MEN_PAGE_SHOP_BY_CATEGORY_BLOCK_ALIGNMENT);
+        await expect(menPage.locators.getShopByCategoryBlock()).toHaveCSS('float', TEST_DATA.MEN_PAGE_SHOP_BY_CATEGORY_BLOCK_ALIGNMENT);
     });
 
     test('Category block contains sub-categories: Tops and Bottoms which are links in blue text', async ({ page }) => {
         const homePage = new HomePage(page);
-        const menPage = new MenPage(page);
+        const menPage = await homePage.clickMenLink();
 
-        await homePage.clickMenLink();
-
-        await expect(menPage.locators.getTopsSubCategoryLink()).toHaveCSS('color', MEN_PAGE_TOPS_SUB_CATEGORY_LINK_COLOR);
-        await expect(menPage.locators.getBottomsSubCategoryLink()).toHaveCSS('color', MEN_PAGE_BOTTOMS_SUB_CATEGORY_LINK_COLOR);
+        await expect(menPage.locators.getTopsSubCategoryLink()).toHaveCSS('color', TEST_DATA.MEN_PAGE_TOPS_SUB_CATEGORY_LINK_COLOR);
+        await expect(menPage.locators.getBottomsSubCategoryLink()).toHaveCSS('color', TEST_DATA.MEN_PAGE_BOTTOMS_SUB_CATEGORY_LINK_COLOR);
     });
 
     test('Tops and Bottoms sub-categories have a counter for items from the right side of the relevant link', async ({ page }) => {
         const homePage = new HomePage(page);
-        const menPage = new MenPage(page);
+        const menPage = await homePage.clickMenLink();
         const subCaregoriesInCategoryBlock = menPage.locators.getSubCaregoriesInCategoryBlock();
 
-        await homePage.clickMenLink();
         await expect(menPage.locators.getTopsSubCategoryLink()).toBeVisible();
         await expect(menPage.locators.getBottomsSubCategoryLink()).toBeVisible();
-        await expect(subCaregoriesInCategoryBlock).toHaveCount(MEN_PAGE_SHOP_BY_CATEGORY_SUB_CATEGORIES_AMOUNT);
+        await expect(subCaregoriesInCategoryBlock).toHaveCount(TEST_DATA.MEN_PAGE_SHOP_BY_CATEGORY_SUB_CATEGORIES_AMOUNT);
 
         for (let itx = 0; itx < await subCaregoriesInCategoryBlock.count(); itx++) {
             let rowArrayValue = (await subCaregoriesInCategoryBlock.nth(itx).textContent()).trim().split('\n');
-            expect(rowArrayValue[0].match(MEN_PAGE_SHOP_BY_CATEGORY_SUB_CATEGORIES_VALUES_REGEX)).toBeTruthy();
-            expect(typeof parseInt(rowArrayValue[1])).toEqual(MEN_PAGE_SHOP_BY_CATEGORY_SUB_CATEGORIES_COUNTER_DATATYPE);
+            expect(rowArrayValue[0].match(TEST_DATA.MEN_PAGE_SHOP_BY_CATEGORY_SUB_CATEGORIES_VALUES_REGEX)).toBeTruthy();
+            expect(typeof parseInt(rowArrayValue[1])).toEqual(TEST_DATA.MEN_PAGE_SHOP_BY_CATEGORY_SUB_CATEGORIES_COUNTER_DATATYPE);
         };
     });
 
-    HOT_SELLERS_NAME.forEach((productsName, idx) => {
+    TEST_MEN_DATA.HOT_SELLERS_NAME.forEach((productsName, idx) => {
         test(`Menu/Men/Hot Sellers Verify user can click on product's name and be redirected to the ${productsName} page`, async ({ page }) => {
             const homePage = new HomePage(page);
 
             const menPage = await homePage.clickMenLink();
             const menHotSellersPage = await menPage.clickMenHotSellersName(productsName);
 
-            await expect(page).toHaveURL(new RegExp(HOT_SELLERS_ENDPOINT_URL[idx]));
-            await expect(menHotSellersPage.locators.getMenName(productsName)).toHaveText(HOT_SELLERS_NAME[idx]);
+            await expect(page).toHaveURL(new RegExp(TEST_MEN_DATA.HOT_SELLERS_ENDPOINT_URL[idx]));
+            await expect(menHotSellersPage.locators.getMenName(productsName)).toHaveText(TEST_MEN_DATA.HOT_SELLERS_NAME[idx]);
         });
     });
 
     test('Verify redirection to Men-Bottoms page from Men page', async ({ page }) => {
         const homePage = new HomePage(page);
-        const menPage = new MenPage(page);
-        const menBottomsPage = new MenBottomsPage(page);
+        const menPage = await homePage.clickMenLink();
 
         await homePage.clickMenLink();
-        await menPage.clickBottomsSideMenuLink();
+        const menBottomsPage = await menPage.clickBottomsSideMenuLink();
 
         await expect(menBottomsPage.locators.getBottomsHeading()).toBeVisible();
-        await expect(page).toHaveURL(BASE_URL + MEN_BOTTOMS_PAGE_END_POINT);
-    })
-    for (const subCategory in MEN_PAGE_SUB_CATEGORY_ENDPOINT_URL) {
+        await expect(page).toHaveURL(TEST_DATA.BASE_URL + TEST_DATA.MEN_BOTTOMS_PAGE_END_POINT);
+    });
+
+    for (const subCategory in TEST_DATA.MEN_PAGE_SUB_CATEGORY_ENDPOINT_URL) {
         test(`${subCategory} sub-category link led to the ${subCategory}-Men page`, async ({ page }) => {
             const homePage = new HomePage(page);
-            const subCategoryPageEndpointUrl = MEN_PAGE_SUB_CATEGORY_ENDPOINT_URL[subCategory];
+            const subCategoryPageEndpointUrl = TEST_DATA.MEN_PAGE_SUB_CATEGORY_ENDPOINT_URL[subCategory];
 
             const menPage = await homePage.clickMenLink();
 
@@ -112,5 +87,5 @@ test.describe('menPage.spec', () => {
             await expect(page).toHaveTitle(`${subCategory} - Men`);
             await expect(page).toHaveURL(new RegExp(subCategoryPageEndpointUrl));
         });
-    }
+    };
 });
