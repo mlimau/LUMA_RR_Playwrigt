203fe5b072a493595981323264546a172e7ae8b5
OlgaPerepelitzki <166035468+OlgaPerepelitzki@users.noreply.github.com>
2024-05-11 14:47:13 +0300

    RF_fixtureShoppingCart_12.4.1 (#564)
    * RF_fixtureShoppingCart_12.4.1
    
    * RF_fixtureShoppingCart_12.4.1_and_skip_menBottomWishList

--- tests/test_page_objects/shoppingCart.spec.js
+++ tests/test_page_objects/shoppingCart.spec.js
@@ -1,30 +1,11 @@
-import { test, expect } from '@playwright/test';
-import HomePage from '../../page_objects/homePage.js';
-import CreateAccountPage from '../../page_objects/createAccountPage.js';
+import { expect } from '@playwright/test';
+import { test } from './base.js';
 import MyAccountPage from '../../page_objects/myAccountPage.js';
-import { FIRST_NAME, LAST_NAME, EMAIL, PASSWORD, PASSWORD_CONFIRM, WOMEN_JACKETS_NAME, BASE_URL, SHOPPING_CART_END_POINT, EMPTY_CARD_MESSAGE } from "../../helpers/testData.js";
+import { WOMEN_JACKETS_NAME, BASE_URL, SHOPPING_CART_END_POINT, EMPTY_CARD_MESSAGE } from "../../helpers/testData.js";
 
 test.describe('shopping Cart', () => {
-    test.beforeEach(async ({ page }) => {
-        const homePage = new HomePage(page);
-        const createAccountPage = new CreateAccountPage(page);
-        const testEmail = EMAIL;
-        await homePage.open();
-        await homePage.clickCreateAccountLink();
-        await createAccountPage.clickFirstNameField();
-        await createAccountPage.fillFirstNameField(FIRST_NAME);
-        await createAccountPage.clickLastNameField();
-        await createAccountPage.fillLastNameField(LAST_NAME);
-        await createAccountPage.clickEmailField();
-        await createAccountPage.fillEmailField(testEmail);
-        await createAccountPage.clickPasswordField();
-        await createAccountPage.fillPasswordField(PASSWORD);
-        await createAccountPage.clickConfirmPasswordField();
-        await createAccountPage.fillConfirmPasswordField(PASSWORD_CONFIRM);
-        await createAccountPage.clickCreateAccountButton();
-    })
 
-    test('Validate link Move to Wish List located on the Shopping Cart page', async ({ page }) => {
+    test('Validate link Move to Wish List located on the Shopping Cart page', async ({ page, createNewAccount}) => {
         const myAccountPage = new MyAccountPage(page);
         await myAccountPage.waitForMyAccountHeader();
         const womenPage = await myAccountPage.clickWomenLink();
@@ -39,7 +20,7 @@ test.describe('shopping Cart', () => {
         await expect(shoppingCartPage.locators.getMoveToWishListLink()).toBeVisible();
     })
 
-    test('Validate the message - the product has been moved to your wish list', async ({ page }) => {
+    test('Validate the message - the product has been moved to your wish list', async ({ page, createNewAccount }) => {
         const myAccountPage = new MyAccountPage(page);
         const womenPage = await myAccountPage.clickWomenLink();
         const jacketsWomenPage = await womenPage.clickWomenJacketsLink();
@@ -54,7 +35,7 @@ test.describe('shopping Cart', () => {
         await expect(shoppingCartPage.locators.getAlerMessageAddToWishList()).toHaveText(`${WOMEN_JACKETS_NAME} has been moved to your wish list.`);
     })
 
-    test('Redirected to the updated Shopping cart page after add item to Wish List', async ({ page }) => {
+    test('Redirected to the updated Shopping cart page after add item to Wish List', async ({ page, createNewAccount }) => {
         const myAccountPage = new MyAccountPage(page);
         const womenPage = await myAccountPage.clickWomenLink();
         const jacketsWomenPage = await womenPage.clickWomenJacketsLink();
