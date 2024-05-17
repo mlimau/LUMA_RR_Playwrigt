aaf2e70b30d6cdf6c0c0886d227ca65c8a8694e4
teya-teya <142177100+teya-teya@users.noreply.github.com>
2024-05-17 15:33:38 +0300

    RF | bottomsWomenPAge.spec.js | TC 14.1.2_08 Ensure each category selected in the "Style" option aligns with the displayed category name on the page. (#587)

--- helpers/testData.js
+++ helpers/testData.js
@@ -28,7 +28,6 @@ export const PIERCE_GYM_SHORT =  "https://magento.softwaretestingboard.com/wishl
 export const SEARCH_ADVANCED_PAGE_END_POINT = '/catalogsearch/advanced/';
 export const PUSH_IT_MESSENGER_BAG_PAGE_END_POINT = '/push-it-messenger-bag.html'
 export const WOMEN_SHORTS_PAGE_END_POINT = '/women/bottoms-women/shorts-women.html'
-export const BOTTOMS_WOMEN_STYLE_BASE_LAYER_PAGE_END_POINT = '/women/bottoms-women.html?style_bottom=104';
 export const FOOTER_ORDERS_AND_RETURNS_PAGE_END_POINT = '/sales/guest/form/';
 export const CUSTOMER_LOGIN_PAGE_END_POINT_SHORT = '/customer/account/login/';
 export const CUSTOMER_WISH_LIST_END_POINT = `/wishlist/index/index/wishlist_id/*`;
--- tests/test_page_objects/bottomsWomenPage.spec.js
+++ tests/test_page_objects/bottomsWomenPage.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import HomePage from "../../page_objects/homePage.js";
-import { BASE_URL, BOTTOMS_WOMEN_PAGE_END_POINT, EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS, BOTTOMS_WOMEN_STYLE_BASE_LAYER_PAGE_END_POINT, WOMEN_BOTTOMS_CATEGORIES_STYLEs_END_POINT, PRODUCT_LIST} from "../../helpers/testData.js";
+import { BASE_URL, BOTTOMS_WOMEN_PAGE_END_POINT, EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS, WOMEN_BOTTOMS_CATEGORIES_STYLEs_END_POINT, PRODUCT_LIST} from "../../helpers/testData.js";
 import { WOMEN_BOTTOMS_CATEGORIES,WOMEN_BOTTOMS_SIZE } from "../../helpers/testWomenData.js";
 
 test.describe('bottomsWomenPage.spec', () => {
@@ -65,22 +65,22 @@ test.describe('bottomsWomenPage.spec', () => {
         });
     });
 
-    test('Ensure the "Base Layer" category selected in the "Style" option aligns with the displayed category name on the page.', async ({ page }) => {
-        const homePage = new HomePage(page);
+    EXPECTED_ITEM_STYLE_WOMEN_BOTTOMS.forEach(async (nameCategory, index) => {
+        test(`Ensure "${nameCategory}" category selected in the "Style" option aligns with the displayed category name on the page.`, async ({ page }) => {
+            const homePage = new HomePage(page);
+            await homePage.hoverWomenMenuitem();
+            const bottomsWomenPage = await homePage.clickBottomsWomenLink();
+            const categories = await bottomsWomenPage.getObjectCategoriesStyle();
 
-        await homePage.hoverWomenMenuitem();
-        const bottomsWomenPage = await homePage.clickBottomsWomenLink();
+            await bottomsWomenPage.clickWomenBottomsOptionStyle();
+            await bottomsWomenPage.clickCategoryStyle(index);
 
-        await expect(page).toHaveURL(BASE_URL + BOTTOMS_WOMEN_PAGE_END_POINT);
-    
-        const categories = await bottomsWomenPage.getObjectCategoriesStyle();
-        await bottomsWomenPage.clickWomenBottomsOptionStyle();
-        await bottomsWomenPage.clickCategoryStyle(0);
-
-        await expect(page).toHaveURL(BASE_URL + BOTTOMS_WOMEN_STYLE_BASE_LAYER_PAGE_END_POINT);
-        await expect(await bottomsWomenPage.locators.getSelectCategory()).toHaveText(await categories[0].name);
+            await expect(page).toHaveURL(BASE_URL + WOMEN_BOTTOMS_CATEGORIES_STYLEs_END_POINT[index]);
+            expect(nameCategory).toEqual(await categories[index].name);
+            await expect(await bottomsWomenPage.locators.getSelectCategory()).toHaveText(await categories[index].name);
+        });
     });
-
+    
     test("User can able to select a category from the suggested list of 2 (two) options: Pants.", async ({ page }) => {
        const homePage = new HomePage(page);
 
